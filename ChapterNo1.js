aceb68d53775fa500931344c5933a5304238a6fd
Muhammad Sufyan <sufyan.vu07@gmail.com>
2024-08-07 14:30:52 +0500

    basic logic building for javascript

--- ChapterNo1.js
+++ ChapterNo1.js
@@ -2,3 +2,10 @@
 
 /* Chapter 1
 Values, Types, and Operators */
+
+// for (let i = 2; i < 3; i++) {
+//     for(let j=1; j<= 10; j++) {  
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// } 
+
